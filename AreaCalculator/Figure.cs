af9336107b2fc800adbbd77ac255715380ba75f8
Temur Melikov <temur212@outlook.com>
2022-03-07 21:09:38 +1100

    Добавьте файлы проекта.

---
+++ AreaCalculator/Figure.cs
@@ -0,0 +1,32 @@
+﻿
+
+namespace AreaCalculator
+{
+    public abstract class Figure
+    {
+        // to store the parameters of a specific figure
+        private readonly IDictionary<string, object> _params; 
+
+        protected Figure(IDictionary<string, object> parameters)
+        {
+            if(!parameters.Any())
+                throw new ArgumentNullException("parameters");
+            _params = parameters; 
+        }
+
+        protected void ValidateRequiredParameters(params string[] parameterNames)
+        {
+            foreach (var name in parameterNames)
+            {
+                if (!_params.TryGetValue(name, out var value))
+                    throw new InvalidOperationException($"Not found required parameter #{name}");
+
+                if (value == null)
+                    throw new ArgumentNullException($"argument #{name} can not be null");
+            }
+        }
+
+        protected abstract double CalculateArea(IDictionary<string, object> param);
+
+    }
+}
